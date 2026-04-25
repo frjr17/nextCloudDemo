2e8e48b07f81d7e66b9be7d97610b36be5198156
Hernán Valencia <hernanadrianv17@gmail.com>
2026-04-24 20:47:47 -0500

    corrigiendo permisos de ejecución

--- demo.sh
+++ demo.sh
@@ -19,7 +19,7 @@ sudo systemctl enable docker
 # Instalando docker-compose
 sudo mkdir -p /usr/libexec/docker/cli-plugins/
 sudo curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m) -o/usr/libexec/docker/cli-plugins/docker-compose
-sudo chmod /x /usr/libexec/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/libexec/docker/cli-plugins/docker-compose
 
 docker compose version
 
